9ab12c02370618e2aab3d0a039282e273324e194
lukedigiovanna <lukedigiovanna@gmail.com>
2024-10-22 01:10:04 -0400

    Start incorporating poll for watching multiple file descriptors

--- src/main.c
+++ src/main.c
@@ -7,6 +7,7 @@
 #include "websocket.h"
 
 #include <sys/socket.h>
+#include <poll.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <arpa/inet.h>
@@ -16,7 +17,7 @@
 #define PORT 8080
 #define MAX_MESSAGE_SIZE 256
 
-static bool handle_client(int client_fd) {
+static bool http_handle_client(int client_fd) {
     struct http_transaction ta;
     http_init_transaction(&ta, client_fd);
     http_parse(&ta);
@@ -31,24 +32,55 @@ int main(int argc, char* argv[]) {
 
     printf("Listening to clients on 127.0.0.1:%d\n", PORT);
 
+    struct pollfd fds[1024]; // this should be dynamic at some point
+    memset(fds, 0, 1024 * sizeof(struct pollfd));
+    nfds_t numfds = 1;
+    fds[0].fd = server_socket_fd;
+    fds[0].events = POLLIN;
+
     for (;;) {
-        // Wait for a connection
-        struct sockaddr_in client_address;
-        socklen_t len;
-        int client_fd = accept(server_socket_fd, (struct sockaddr*) &client_address, &len);
+        int ready = poll(fds, numfds, -1);
 
-        char buf[INET_ADDRSTRLEN];
-        inet_ntop(AF_INET, &(client_address.sin_addr), buf, INET_ADDRSTRLEN);
-
-        fprintf(stderr, "Received client: %s:%d\n", buf, ntohs(client_address.sin_port));
-
-        bool keep_alive = http_handle_client(client_fd);
-
-        if (!keep_alive) {
-            close(client_fd);
+        if (ready == -1) {
+            perror("poll");
+            break;
         }
-        else {
-            recv(client_fd, )
+
+        for (nfds_t i = 0; i < numfds; i++) {
+            if (fds[i].events != 0) {
+                if (fds[i].fd == server_socket_fd) {
+                    // Wait for a connection
+                    struct sockaddr_in client_address;
+                    socklen_t len;
+                    int client_fd = accept(server_socket_fd, (struct sockaddr*) &client_address, &len);
+
+                    char buf[INET_ADDRSTRLEN];
+                    inet_ntop(AF_INET, &(client_address.sin_addr), buf, INET_ADDRSTRLEN);
+
+                    fprintf(stderr, "Received client: %s:%d\n", buf, ntohs(client_address.sin_port));
+
+                    bool keep_alive = http_handle_client(client_fd);
+
+                    if (!keep_alive) {
+                        close(client_fd);
+                    }
+                    else {
+                        // Add the client to the list of file descriptors to watch
+                        fds[numfds].fd = client_fd;
+                        fds[numfds].events = POLLIN;
+                        numfds++;
+                    }
+                }
+                else { // Assume from client file descriptor
+                    char buff[1024];
+                    int bread = recv(fds[i].fd, buff, 1024, 0);
+                    if (bread < 0) {
+                        perror("recv");
+                    }
+                    buff[bread] = '\0';
+                    printf("received: %s\n", buff);
+                }
+            }
         }
     }
 
